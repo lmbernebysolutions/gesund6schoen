0ef66e99600f74a7ea3c1077552a9f85e9bd4b41
lmbernebysolutions <l.m@bernebysolutions.de>
2025-11-25 14:49:14 +0100

    chore: Remove password protection for production

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import PasswordProtection from './components/PasswordProtection.jsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PasswordProtection>
-      <App />
-    </PasswordProtection>
+    <App />
   </React.StrictMode>,
 )
